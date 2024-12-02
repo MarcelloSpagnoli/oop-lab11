9c5915534d5ba42309bb4354b498480550f68ee3
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2024-12-02 20:43:57 +0100

    exercise 113 seems to work, gotta be checked by prof and static analyzer

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -1,6 +1,7 @@
 package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
 
 import javax.swing.JButton;
@@ -14,4 +15,97 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class ConcurrentGUI extends JFrame {
+    
+
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel("0");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+
+    public ConcurrentGUI(){
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        final Agent agent = new Agent();
+
+        stop.addActionListener(e -> {
+            agent.stopCounting();
+            stop.setEnabled(false);
+            up.setEnabled(false);
+            down.setEnabled(false);
+        });
+
+        up.addActionListener(e -> {
+            agent.goUp();
+        });
+
+        down.addActionListener(e -> {
+            agent.goDown();
+        });
+        
+        agent.run();
+    }
+
+
+    private class Agent implements Runnable {
+        final static Boolean UP = true;
+        final static Boolean DOWN = false;
+
+        private volatile boolean stop;
+        private int counter;
+        private boolean direction;
+        private volatile boolean started;
+
+        @Override
+        public void run() {
+            while(!started){}
+            while (!this.stop) {
+                try {final var nextText = Integer.toString(this.counter);
+                SwingUtilities.invokeLater(()->ConcurrentGUI.this.display.setText(nextText));
+                if( direction == UP) {
+                    counter++;
+                }else {
+                    counter--;
+                }
+                Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace(); //NOPMD
+                }
+            } 
+        }
+
+        /**
+         * Stops the counter,
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+        
+        /**
+         * Makes the counter count up.
+         */
+        public void goUp(){
+            started = true;
+            this.direction = UP;
+        }
+
+        /**
+         * Makes the counter count down.
+         */
+        public void goDown(){
+            started = true;
+            this.direction = DOWN;
+        }
+    }
 }
--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -1,11 +1,9 @@
 package it.unibo.oop.reactivegui03;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.TimeUnit;
+import java.text.DecimalFormat;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,8 +12,134 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
- * Third experiment with reactive gui.
+ * Second example of reactive GUI.
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class AnotherConcurrentGUI extends JFrame {
+    
+    private static final double WIDTH_PERC = 0.3;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel("0");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+    private final JLabel timeDisplay = new JLabel("0.0");
+    private Agent agent = new Agent();
+    private TimeAgent timeAgent = new TimeAgent();
+
+    public AnotherConcurrentGUI(){
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        timeDisplay.setEnabled(false);
+        display.setEnabled(false);
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        panel.add(timeDisplay);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        
+
+        stop.addActionListener(e -> {
+            this.stop();
+        });
+
+        up.addActionListener(e -> {
+            agent.goUp();
+        });
+
+        down.addActionListener(e -> {
+            agent.goDown();
+        });
+        
+        new Thread(agent).start();
+    }
+
+    private void stop(){
+        agent.stopCounting();
+        stop.setEnabled(false);
+        up.setEnabled(false);
+        down.setEnabled(false);
+    }
+
+
+    private class Agent implements Runnable {
+        final static Boolean UP = true;
+        final static Boolean DOWN = false;
+
+        private volatile boolean stop;
+        private int counter;
+        private boolean direction;
+        private volatile boolean started;
+
+        @Override
+        public void run() {
+            while(!started){}
+            new Thread(timeAgent).start();
+            while (!this.stop) {
+                try {
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeLater(()->AnotherConcurrentGUI.this.display.setText(nextText));
+                    if( direction == UP) {
+                        counter++;
+                    }else {
+                        counter--;
+                    }
+                Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace(); //NOPMD
+                }
+            } 
+        }
+
+        /**
+         * Stops the counter,
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+        
+        /**
+         * Makes the counter count up.
+         */
+        public void goUp(){
+            started = true;
+            this.direction = UP;
+        }
+
+        /**
+         * Makes the counter count down.
+         */
+        public void goDown(){
+            started = true;
+            this.direction = DOWN;
+        }
+    }
+
+    private class TimeAgent implements Runnable {
+        private static double TIME_TO_ELAPSE_SECONDS = 10;
+        @Override
+        public void run() {
+            try {
+                final long t0 = System.currentTimeMillis();
+                Double elapsed;
+                do  {
+                    final long t1 = System.currentTimeMillis();
+                    elapsed = (t1 - t0)/1000.0;
+                    final String nextText = new DecimalFormat("0.0").format(elapsed);
+                    SwingUtilities.invokeLater(()-> 
+                        AnotherConcurrentGUI.this.timeDisplay.setText(nextText));
+                    Thread.sleep(10);
+                } while (elapsed < TIME_TO_ELAPSE_SECONDS);
+                AnotherConcurrentGUI.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(); //NOPMD
+            }
+        }
+    }
 }
